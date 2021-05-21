566065ed2776ddecd2037fce2242b44d53973f5f
jVictor <victor.jvas1@gmail.com>
2021-05-21 01:09:22 -0300

    Add project files.

---
+++ Mongo-API/Controllers/InfectedController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mongo_API.Data.Collections;
+using Mongo_API.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongo_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectedController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infected> _infectedCollection;
+
+        public InfectedController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectedCollection = _mongoDB.DB.GetCollection<Infected>(typeof(Infected).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult<Infected> CreateInfected([FromBody] InfectedDto dto)
+        {
+            var infected = new Infected(dto.birthDate, dto.Gender, dto.Latitude, dto.Longitude);
+
+            _infectedCollection.InsertOne(infected);
+
+            return Created(nameof(CreateInfected), infected);
+        }
+
+        [HttpGet]
+        public ActionResult GetInfecteds(){
+
+            var infecteds = _infectedCollection.Find(Builders<Infected>.Filter.Empty).ToList();
+
+            return Ok(infecteds);
+        }
+
+        [HttpGet("{gender}")]
+        public ActionResult GetInfectedByGender([FromRoute] string gender)
+        {
+            var infected = _infectedCollection.Find(x=> x.Gender == gender);
+            return Ok(infected);
+        }
+
+    }
+}
---
+++ Mongo-API/Data/Collections/Infected.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongo_API.Data.Collections
+{
+    public class Infected
+    {
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public GeoJson2DGeographicCoordinates Location { get; set; }
+
+        public Infected(DateTime birthDate, string gender, double latitude, double longitude)
+        {
+            BirthDate = birthDate;
+            Gender = gender;
+            Location = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+    }
+}
